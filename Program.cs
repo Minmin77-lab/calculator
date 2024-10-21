1e113d89d7540546d1236f07f08bcda274c6e3de
Minmin77-lab <panahovaarina52@gmail.com>
2024-10-21 18:22:54 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Действие:");
+string? action = Console.ReadLine();
+Console.WriteLine("Второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result;
+if (action == "+")
+{
+    result = num1 + num2;
+    Console.WriteLine(result);
+}
+else if (action == "*")
+{
+    result = num1 * num2;
+    Console.WriteLine(result);
+}
